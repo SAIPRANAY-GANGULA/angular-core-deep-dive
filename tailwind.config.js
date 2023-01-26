837834ce06449e7f952c8d763747baca74af6171
PranayBackbase <saig@backbase.com>
2023-01-26 21:10:05 +0530

    fix tailwind installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./src/**/*.{html,ts}"],
+  },
   theme: {
     extend: {},
   },
