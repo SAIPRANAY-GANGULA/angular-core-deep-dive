50c522e1191fb8dab0117589d0f5fa0e0ae03fc7
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-02-10 16:37:35 +0530

    Tree Shakeable Providers: any(Many Instances)

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -12,7 +12,8 @@ let counter = 1;
 export const coursesProvider = (http: HttpService) => new CoursesService(http);
 
 @Injectable({
-  providedIn: 'root', // Tree Shakable, if not injected anywhere it will not be included in the bundle
+  providedIn: 'any', // Tree Shakable & 'n' instances are created,
+  // where 'n' is no of lazy loaded modules in which service is used
 })
 export class CoursesService {
   constructor(private http: HttpService) {
